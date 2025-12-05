c11e47318f81998520d73389f6a6fd44d80f1b5e
arponkarangtip <arponkarangtip@gmail.com>
2025-12-05 19:05:20 +0600

    This is Project Database

---
+++ SQL File/hostel.sql
@@ -0,0 +1,400 @@
+-- phpMyAdmin SQL Dump
+-- version 5.2.1
+-- https://www.phpmyadmin.net/
+--
+-- Host: localhost
+-- Generation Time: Dec 04, 2025 at 02:25 PM
+-- Server version: 10.4.28-MariaDB
+-- PHP Version: 8.2.4
+
+SET SQL_MODE = "NO_AUTO_VALUE_ON_ZERO";
+START TRANSACTION;
+SET time_zone = "+00:00";
+
+
+/*!40101 SET @OLD_CHARACTER_SET_CLIENT=@@CHARACTER_SET_CLIENT */;
+/*!40101 SET @OLD_CHARACTER_SET_RESULTS=@@CHARACTER_SET_RESULTS */;
+/*!40101 SET @OLD_COLLATION_CONNECTION=@@COLLATION_CONNECTION */;
+/*!40101 SET NAMES utf8mb4 */;
+
+--
+-- Database: `hostel`
+--
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `admin`
+--
+
+CREATE TABLE `admin` (
+  `id` int(11) NOT NULL,
+  `username` varchar(255) NOT NULL,
+  `email` varchar(255) NOT NULL,
+  `password` varchar(300) NOT NULL,
+  `reg_date` timestamp NOT NULL DEFAULT current_timestamp(),
+  `updation_date` date NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=latin1 COLLATE=latin1_swedish_ci;
+
+--
+-- Dumping data for table `admin`
+--
+
+INSERT INTO `admin` (`id`, `username`, `email`, `password`, `reg_date`, `updation_date`) VALUES
+(1, 'admin', 'admin@gmail.com', 'Test@1234', '2024-01-31 20:31:45', '2024-01-01');
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `adminlog`
+--
+
+CREATE TABLE `adminlog` (
+  `id` int(11) NOT NULL,
+  `adminid` int(11) NOT NULL,
+  `ip` varbinary(16) NOT NULL,
+  `logintime` timestamp NOT NULL DEFAULT current_timestamp()
+) ENGINE=InnoDB DEFAULT CHARSET=latin1 COLLATE=latin1_swedish_ci;
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `complainthistory`
+--
+
+CREATE TABLE `complainthistory` (
+  `id` int(11) NOT NULL,
+  `complaintid` int(11) DEFAULT NULL,
+  `compalintStatus` varchar(255) DEFAULT NULL,
+  `complaintRemark` mediumtext DEFAULT NULL,
+  `postingDate` timestamp NULL DEFAULT current_timestamp()
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `complaints`
+--
+
+CREATE TABLE `complaints` (
+  `id` int(11) NOT NULL,
+  `ComplainNumber` bigint(12) DEFAULT NULL,
+  `userId` int(11) DEFAULT NULL,
+  `complaintType` varchar(255) DEFAULT NULL,
+  `complaintDetails` mediumtext DEFAULT NULL,
+  `complaintDoc` varchar(255) DEFAULT NULL,
+  `complaintStatus` varchar(255) DEFAULT NULL,
+  `registrationDate` timestamp NULL DEFAULT current_timestamp()
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
+
+--
+-- Dumping data for table `complaints`
+--
+
+INSERT INTO `complaints` (`id`, `ComplainNumber`, `userId`, `complaintType`, `complaintDetails`, `complaintDoc`, `complaintStatus`, `registrationDate`) VALUES
+(1, 389685413, 5, 'Electrical', 'Switch not working.', NULL, 'Closed', '2025-01-14 11:10:24');
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `courses`
+--
+
+CREATE TABLE `courses` (
+  `id` int(11) NOT NULL,
+  `course_code` varchar(255) DEFAULT NULL,
+  `course_sn` varchar(255) DEFAULT NULL,
+  `course_fn` varchar(255) DEFAULT NULL,
+  `posting_date` timestamp NULL DEFAULT current_timestamp()
+) ENGINE=InnoDB DEFAULT CHARSET=latin1 COLLATE=latin1_swedish_ci;
+
+--
+-- Dumping data for table `courses`
+--
+
+INSERT INTO `courses` (`id`, `course_code`, `course_sn`, `course_fn`, `posting_date`) VALUES
+(1, '111', 'Meditation', 'Mediation', '2025-11-29 16:46:43');
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `feedback`
+--
+
+CREATE TABLE `feedback` (
+  `id` int(11) NOT NULL,
+  `AccessibilityWarden` varchar(255) DEFAULT NULL,
+  `AccessibilityMember` varchar(255) DEFAULT NULL,
+  `RedressalProblem` varchar(255) DEFAULT NULL,
+  `Room` varchar(255) DEFAULT NULL,
+  `Mess` varchar(255) DEFAULT NULL,
+  `HostelSurroundings` varchar(255) DEFAULT NULL,
+  `OverallRating` varchar(255) DEFAULT NULL,
+  `FeedbackMessage` varchar(255) DEFAULT NULL,
+  `userId` int(11) DEFAULT NULL,
+  `postinDate` timestamp NULL DEFAULT current_timestamp()
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `registration`
+--
+
+CREATE TABLE `registration` (
+  `id` int(11) NOT NULL,
+  `roomno` int(11) DEFAULT NULL,
+  `seater` int(11) DEFAULT NULL,
+  `feespm` int(11) DEFAULT NULL,
+  `foodstatus` int(11) DEFAULT NULL,
+  `stayfrom` date DEFAULT NULL,
+  `duration` int(11) DEFAULT NULL,
+  `course` varchar(500) DEFAULT NULL,
+  `regno` int(11) DEFAULT NULL,
+  `firstName` varchar(500) DEFAULT NULL,
+  `middleName` varchar(500) DEFAULT NULL,
+  `lastName` varchar(500) DEFAULT NULL,
+  `gender` varchar(250) DEFAULT NULL,
+  `contactno` bigint(11) DEFAULT NULL,
+  `emailid` varchar(500) DEFAULT NULL,
+  `egycontactno` bigint(11) DEFAULT NULL,
+  `guardianName` varchar(500) DEFAULT NULL,
+  `guardianRelation` varchar(500) DEFAULT NULL,
+  `guardianContactno` bigint(11) DEFAULT NULL,
+  `corresAddress` varchar(500) DEFAULT NULL,
+  `corresCIty` varchar(500) DEFAULT NULL,
+  `corresState` varchar(500) DEFAULT NULL,
+  `corresPincode` int(11) DEFAULT NULL,
+  `pmntAddress` varchar(500) DEFAULT NULL,
+  `pmntCity` varchar(500) DEFAULT NULL,
+  `pmnatetState` varchar(500) DEFAULT NULL,
+  `pmntPincode` int(11) DEFAULT NULL,
+  `postingDate` timestamp NULL DEFAULT current_timestamp(),
+  `updationDate` varchar(500) DEFAULT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=latin1 COLLATE=latin1_swedish_ci;
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `rooms`
+--
+
+CREATE TABLE `rooms` (
+  `id` int(11) NOT NULL,
+  `seater` int(11) DEFAULT NULL,
+  `room_no` int(11) DEFAULT NULL,
+  `fees` int(11) DEFAULT NULL,
+  `posting_date` timestamp NULL DEFAULT current_timestamp()
+) ENGINE=InnoDB DEFAULT CHARSET=latin1 COLLATE=latin1_swedish_ci;
+
+--
+-- Dumping data for table `rooms`
+--
+
+INSERT INTO `rooms` (`id`, `seater`, `room_no`, `fees`, `posting_date`) VALUES
+(1, 5, 100, 8000, '2025-01-01 22:45:43'),
+(2, 2, 201, 6000, '2025-01-01 22:45:43'),
+(3, 2, 200, 6000, '2025-01-01 22:45:43'),
+(4, 3, 112, 4000, '2025-01-01 22:45:43'),
+(5, 5, 132, 2000, '2025-01-01 22:45:43'),
+(6, 3, 145, 3000, '2025-01-01 22:45:43');
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `states`
+--
+
+CREATE TABLE `states` (
+  `id` int(11) NOT NULL,
+  `State` varchar(150) DEFAULT NULL
+) ENGINE=MyISAM DEFAULT CHARSET=latin1 COLLATE=latin1_swedish_ci;
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `userlog`
+--
+
+CREATE TABLE `userlog` (
+  `id` int(11) NOT NULL,
+  `userId` int(11) NOT NULL,
+  `userEmail` varchar(255) NOT NULL,
+  `userIp` varbinary(16) NOT NULL,
+  `city` varchar(255) NOT NULL,
+  `country` varchar(255) NOT NULL,
+  `loginTime` timestamp NOT NULL DEFAULT current_timestamp()
+) ENGINE=InnoDB DEFAULT CHARSET=latin1 COLLATE=latin1_swedish_ci;
+
+--
+-- Dumping data for table `userlog`
+--
+
+INSERT INTO `userlog` (`id`, `userId`, `userEmail`, `userIp`, `city`, `country`, `loginTime`) VALUES
+(1, 1, 'arpon@gmail.com', 0x3a3a31, '', '', '2025-11-29 16:48:43'),
+(2, 1, 'arpon@gmail.com', 0x3a3a31, '', '', '2025-11-30 12:57:44'),
+(3, 1, 'arpon@gmail.com', 0x3a3a31, '', '', '2025-11-30 16:27:03'),
+(4, 1, 'arpon@gmail.com', 0x3a3a31, '', '', '2025-12-04 12:36:04');
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `userregistration`
+--
+
+CREATE TABLE `userregistration` (
+  `id` int(11) NOT NULL,
+  `regNo` varchar(255) DEFAULT NULL,
+  `firstName` varchar(255) DEFAULT NULL,
+  `middleName` varchar(255) DEFAULT NULL,
+  `lastName` varchar(255) DEFAULT NULL,
+  `gender` varchar(255) DEFAULT NULL,
+  `contactNo` bigint(20) DEFAULT NULL,
+  `email` varchar(255) DEFAULT NULL,
+  `password` varchar(255) DEFAULT NULL,
+  `regDate` timestamp NULL DEFAULT current_timestamp(),
+  `updationDate` varchar(45) DEFAULT NULL,
+  `passUdateDate` varchar(45) DEFAULT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=latin1 COLLATE=latin1_swedish_ci;
+
+--
+-- Dumping data for table `userregistration`
+--
+
+INSERT INTO `userregistration` (`id`, `regNo`, `firstName`, `middleName`, `lastName`, `gender`, `contactNo`, `email`, `password`, `regDate`, `updationDate`, `passUdateDate`) VALUES
+(1, '011221180', 'Arpon ', 'Kar', 'Angtip', 'male', 1781414123, 'arpon@gmail.com', '123456', '2025-11-29 16:48:17', NULL, NULL);
+
+--
+-- Indexes for dumped tables
+--
+
+--
+-- Indexes for table `admin`
+--
+ALTER TABLE `admin`
+  ADD PRIMARY KEY (`id`);
+
+--
+-- Indexes for table `complainthistory`
+--
+ALTER TABLE `complainthistory`
+  ADD PRIMARY KEY (`id`);
+
+--
+-- Indexes for table `complaints`
+--
+ALTER TABLE `complaints`
+  ADD PRIMARY KEY (`id`);
+
+--
+-- Indexes for table `courses`
+--
+ALTER TABLE `courses`
+  ADD PRIMARY KEY (`id`);
+
+--
+-- Indexes for table `feedback`
+--
+ALTER TABLE `feedback`
+  ADD PRIMARY KEY (`id`);
+
+--
+-- Indexes for table `registration`
+--
+ALTER TABLE `registration`
+  ADD PRIMARY KEY (`id`);
+
+--
+-- Indexes for table `rooms`
+--
+ALTER TABLE `rooms`
+  ADD PRIMARY KEY (`id`),
+  ADD KEY `room_no` (`room_no`);
+
+--
+-- Indexes for table `states`
+--
+ALTER TABLE `states`
+  ADD PRIMARY KEY (`id`);
+
+--
+-- Indexes for table `userlog`
+--
+ALTER TABLE `userlog`
+  ADD PRIMARY KEY (`id`);
+
+--
+-- Indexes for table `userregistration`
+--
+ALTER TABLE `userregistration`
+  ADD PRIMARY KEY (`id`),
+  ADD KEY `email` (`email`);
+
+--
+-- AUTO_INCREMENT for dumped tables
+--
+
+--
+-- AUTO_INCREMENT for table `admin`
+--
+ALTER TABLE `admin`
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=2;
+
+--
+-- AUTO_INCREMENT for table `complainthistory`
+--
+ALTER TABLE `complainthistory`
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT;
+
+--
+-- AUTO_INCREMENT for table `complaints`
+--
+ALTER TABLE `complaints`
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=2;
+
+--
+-- AUTO_INCREMENT for table `courses`
+--
+ALTER TABLE `courses`
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=2;
+
+--
+-- AUTO_INCREMENT for table `feedback`
+--
+ALTER TABLE `feedback`
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT;
+
+--
+-- AUTO_INCREMENT for table `registration`
+--
+ALTER TABLE `registration`
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT;
+
+--
+-- AUTO_INCREMENT for table `rooms`
+--
+ALTER TABLE `rooms`
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=7;
+
+--
+-- AUTO_INCREMENT for table `states`
+--
+ALTER TABLE `states`
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT;
+
+--
+-- AUTO_INCREMENT for table `userlog`
+--
+ALTER TABLE `userlog`
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=5;
+
+--
+-- AUTO_INCREMENT for table `userregistration`
+--
+ALTER TABLE `userregistration`
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=2;
+COMMIT;
+
+/*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
+/*!40101 SET CHARACTER_SET_RESULTS=@OLD_CHARACTER_SET_RESULTS */;
+/*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
